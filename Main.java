41cdadbcb35306ab815e8474d7c03a945fd19fe1
mchoi5740 <31289950+mchoi5740@users.noreply.github.com>
2018-10-12 12:22:56 -0700

    10/12/18
    10/12/18

--- Main.java
+++ Main.java
@@ -158,7 +158,7 @@ class Main
     for (int i=0; i<ar1.length; i++)
     {
         if (ar1[i]%2==1)
-            ar1[i]=ar1*10;
+            ar1[i]=ar1[i]*10;
     }
     for (int i=0; i<ar1.length; i++)
         System.out.println("ar1["+i+"]="+ar1[i]);
@@ -174,6 +174,9 @@ class Main
           *    ar2[3]=3  ->  ar2odds[1]=3
           */
  System.out.println("***Task 8***");
+ int[] ar2odds= new int[ar2.length];
+ 
+ 
     
         /*
          * Task 9. In the array ar2, count how many odd numbers you
